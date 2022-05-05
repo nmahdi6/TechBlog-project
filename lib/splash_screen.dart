4336d7a675d77e5cc7ebbe1204a002132933dba2
nmahdi6 <nmahdi600@gmail.com>
2022-05-06 04:11:47 +0430

    create main_screen and go main_screen page befor 3 min

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,9 +1,34 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:tec/gen/assets.gen.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:tec/main_screen.dart';
 import 'package:tec/my_colors.dart';
 
-class splashScreen extends StatelessWidget {
+class splashScreen extends StatefulWidget {
+  @override
+  State<splashScreen> createState() => _splashScreenState();
+}
+
+class _splashScreenState extends State<splashScreen> {
+
+  @override
+  void initState(){
+
+    Future.delayed(Duration(seconds: 3)).then((value){
+
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: ((context) => MainScreen())
+        ));
+
+    });
+
+    super.initState();
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
